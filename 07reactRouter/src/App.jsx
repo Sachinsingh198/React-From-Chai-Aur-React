703f105a8f96bd90a79f2cb060fcc514fd502466
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-03-26 23:35:13 +0530

    adding router

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Header from "./components/Header/Header.jsx";
+import Home from "./components/Home/Home.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+
+function App() {
+
+
+  return (
+    <>
+        <Header/>
+        <Home/>
+        <Footer/>
+    </>
+  )
+}
+
+export default App
