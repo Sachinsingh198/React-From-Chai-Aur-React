c0434083ef1a0dfd225f9d33d3c1362e8737dee8
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-04-15 22:31:48 +0530

    stats card

--- SelfStudy/02_IotDeviceMonitor/src/App.jsx
+++ SelfStudy/02_IotDeviceMonitor/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import Header from "./components/Header.jsx";
+import StatsCard from "./components/StatsCard.jsx";
+import { Trash2, AlertTriangle, Wifi } from 'lucide-react';
 
 function App() {
 
@@ -9,24 +11,31 @@ function App() {
       <>
           <div className="min-h-screen bg-slate-900 p-8">
               <Header isLive = {isLive}/>
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-10">
+                  <StatsCard
+                      label="Total Bins"
+                      value="03"
+                      icon={Trash2}
+                      colorClass="text-sky-400"
+                  />
+                  <StatsCard
+                      label="Action Needed"
+                      value="01"
+                      icon={AlertTriangle}
+                      colorClass="text-amber-400"
+                  />
+                  <StatsCard
+                      label="Network"
+                      value="Optimal"
+                      icon={Wifi}
+                      colorClass="text-emerald-400"
+                  />
+              </div>
+
           </div>
 
         {/*/!*Header *!/*/}
         {/*<div>*/}
-
-        {/*  /!*Main Header *!/*/}
-        {/*  <div>*/}
-        {/*    <h1>*/}
-        {/*      <span>WaMa</span> IoT Device Monitor*/}
-        {/*    </h1>*/}
-        {/*      <p>*/}
-        {/*          Real Time Waste Analytics for your Raspberry Pi*/}
-        {/*      </p>*/}
-        {/*  </div>*/}
-
-        {/*  /!*Live Tracking Logo *!/*/}
-        {/*  <h2>Start Live Tracking</h2>*/}
-        {/*</div>*/}
 
         {/*  /!*Stats Row *!/*/}
         {/*  <div>*/}
---
+++ SelfStudy/02_IotDeviceMonitor/src/components/StatsCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const StatsCard = ({label, value, icon : Icon, colorClass}) => {
+    return (
+        <div className="flex items-center bg-slate-800/50 rounded-2xl border-slate-700 shadow-xl gap-2 m-4 p-3">
+            <div className={`rounded-xl bg-slate-500 p-5 m-4 ${colorClass}`}>
+                {/*Icon */}
+                <Icon size = {24}/>
+            </div>
+            <div>
+                <p className="text-slate-400 text-xs font-bold uppercase tracking-widest">{label}</p>
+                <p className="text-xl font-black text-white mt-1">{value}</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default StatsCard;
